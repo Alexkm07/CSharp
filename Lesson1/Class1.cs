13274ee28076be63305911142e5598b951f09eef
Alexkm07 <Alexkm@inbox.ru>
2018-11-23 15:00:50 +0300

    Выполненное задание

---
+++ Lesson1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+    static class HomeWorkClass
+    {
+        public static void Pause()
+        {
+            Console.ReadKey();
+        }
+
+        public static void Print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+    }
+}
